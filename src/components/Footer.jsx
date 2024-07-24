b187f2ab2013e09da358921886438236e6c2ece8
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-07-24 19:23:38 +0200

    feat
    finish footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,14 @@
-<footer class="h-20 flex items-center justify-center flex-shrink-0">
-    <div class="container mx-auto text-center">
-        <p class="text-blue-950">© 2024 Firecross. All rights reserved.</p>
-    </div>
-</footer>
+function Footer() {
+    return (
+
+        <footer class="h-20 flex items-center justify-center flex-shrink-0">
+            <div class="container mx-auto text-center">
+                <p class="text-blue-950">© 2024 Firecross. All rights reserved.</p>
+            </div>
+        </footer>
+
+
+    )
+}
+
+export default Footer
