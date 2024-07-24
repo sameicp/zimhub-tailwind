d1ac508dc49bb882380ff5411ce2c66142d69bc8
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-07-24 19:28:39 +0200

    wip
    remove p tag and add Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+
+
 export default function App() {
   return (
     <div>
       <body className="flex flex-col min-h-screen">
-        <p className="font-sans text-blue-500 text-2xl">nav section</p>
+        <Navbar />
         <main className="flex-grow">
         </main>
         <p className="font-sans text-blue-500 text-2xl">footer section</p>
