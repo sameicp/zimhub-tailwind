5ce8f14b8af2e1464ca11fcf0252000440d77eb7
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-07-24 19:20:13 +0200

    wip
    footer and nav tailwind code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 export default function App() {
   return (
-    <body className="flex flex-col min-h-screen">
-
-    </body>
+    <div>
+      <body className="flex flex-col min-h-screen">
+        <p className="font-sans text-blue-500 text-2xl">nav section</p>
+        <main className="flex-grow">
+        </main>
+        <p className="font-sans text-blue-500 text-2xl">footer section</p>
+      </body>
+    </div>
   )
 }
