8b73f6294f35e22dce89c78fe3b226352704391f
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-07-24 19:08:42 +0200

    wip
    app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <body className="flex flex-col min-h-screen">
+
+    </body>
   )
 }
