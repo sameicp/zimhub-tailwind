7bbdcff5b74905da2bf3bf8e68f429bacb834729
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-07-26 12:54:39 +0200

    del
    mybackup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,7 @@
 import Navbar from "../components/Navbar";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faConciergeBell, faPhone } from '@fortawesome/free-solid-svg-icons'
+
 
 function Home() {
     return (
@@ -9,10 +12,24 @@ function Home() {
             <Navbar />
 
             <div className="flex justify-between">
-                <div>
-                    <h1 className="text-7xl mt-28">King Vanq Murisei henyu .</h1>
+                <div className=" w-11/12">
+                    <h1 className="text-7xl mt-28 font-semibold">Welcome To Zim ICP Innovation Hub.</h1>
+                    <p className="font-bold mt-5">Let us build cool sultions that live on the icp blockchain
+                        and solve people problems
+                    </p>
+                    <div className="flex space-x-10 mt-5">
+                        <button className="bg-blacky text-white px-5 py-2 rounded-2xl flex items-center space-x-2">
+                            <FontAwesomeIcon icon={faConciergeBell} />
+                            <span>Our Services</span>
+                        </button>
+                        <button className="flex items-center space-x-2">
+                            <FontAwesomeIcon icon={faPhone} />
+                            <span>Call Us</span>
+                        </button>
+                    </div>
+
                 </div>
-                <img src="/bg.jpg" className="w-6/12 mt-8 rounded" alt="No Image" />
+                <img src="/bg.jpg" className="w-6/12 mt-16 rounded-full" alt="No Image" />
 
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ export default {
         'bluey': '#3433fc',
         'dark': '#1a0936',
         'blu': '#b25ffb',
+        'goldy': "#f5b0aa"
       }
     },
   },
