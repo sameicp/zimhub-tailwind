c0d2177453d9ee7008b3d047e6dab70906fc736c
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-07-26 08:10:35 +0200

    wip
    fix Footer and App

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 
 
-export default function App() {
+function App() {
   return (
     <div>
       <body className="flex flex-col min-h-screen bg-slate-50 dark:bg-black dark:text-white">
@@ -15,5 +15,6 @@ export default function App() {
       </body>
     </div>
   )
+}
 
 export default App
