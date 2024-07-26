32cbf60e3096df671ed9f636beb5a5fc5e3e6592
sameicp <same.eth2001@gmail.com>
2024-07-26 14:20:33 +0200

    fixed layouts for wider screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,25 +11,25 @@ function Home() {
         >
             <Navbar />
 
-            <div className="flex justify-between">
+            <div className="container flex justify-between mx-auto items-center">
                 <div className=" w-11/12">
-                    <h1 className="text-7xl mt-28 font-semibold">Welcome To Zim ICP Innovation Hub.</h1>
-                    <p className="font-bold mt-5">Let us build cool sultions that live on the icp blockchain
+                    <h1 className="text-7xl mt-28 font-semibold">Welcome To Zim<br /> ICP Innovation Hub.</h1>
+                    <p className="font-bold mt-5">Let us build cool solutions that live on the icp blockchain
                         and solve people problems
                     </p>
                     <div className="flex space-x-10 mt-5">
-                        <button className="bg-gradient-to-r from-blacky to-blu2 text-white px-5 py-2 rounded-2xl flex items-center space-x-2">
+                        <button className="bg-gradient-to-r from-blacky to-blu2 text-white px-5 py-2 rounded-2xl flex items-center space-x-2 text-2xl hover:opacity-90">
                             <FontAwesomeIcon icon={faConciergeBell} />
                             <span>Our Services</span>
                         </button>
-                        <button className="flex items-center space-x-2">
+                        <button className="flex items-center space-x-2 text-2xl hover:opacity-90">
                             <FontAwesomeIcon icon={faPhone} />
                             <span>Call Us</span>
                         </button>
                     </div>
 
                 </div>
-                <img src="/p2.jpg" className="w-6/12 mt-16 rounded-full" alt="No Image" />
+                <img src="./assets/group-photo.jpg" className="w-6/12 mt-16 rounded-full" alt="No Image" />
 
             </div>
         </div>
