4a957ceceadb33fc9d98dca4e0d1df77a42bfbd3
sameicp <same.eth2001@gmail.com>
2024-07-26 04:08:45 +0200

    wip:  homepage and  added Blog component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
 
 
 export default function App() {
   return (
     <div>
-      <body className="flex flex-col min-h-screen">
+      <body className="flex flex-col min-h-screen bg-slate-50 dark:bg-black dark:text-white">
         <Navbar />
         <main className="flex-grow">
-          <p>The home part of the page</p>
+          <Home />
         </main>
         <Footer />
       </body>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,33 @@
+import Blog from "../components/Blog"
 
 function Home() {
   return (
     <main className="max-w-screen-xl mx-auto">
-        <section id="hero" className="flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12 scroll-mt-40 widescreen:section-min-height tallscreen:section-min-height">
-            <article className="sm:w-1/2">
+        <section id="hero" className="flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-6 my-12 scroll-mt-40">
+            <article className="sm:w-1/3">
                 <h2 className="max-w-md text-4xl font-bold text-center sm:max-w-lg sm:text-5xl sm:text-left text-slate-900 dark:text-white">
                     Unleashing Potential, Empowering Innovations on the <span className="text-indigo-700 dark:text-indigo-300">Internet Computer Protocol</span>...
                 </h2>
-                <p className="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
+                <p className="max-w-md text-2xl my-6 text-center sm:text-left text-slate-700 dark:text-slate-400">
                     We are accelerating the Internet Computer mass adoption in Zimbabwe
                 </p>
-                <a href="" className="max-w-md text-2xl mt-8 text-center sm:text-left text-slate-700 dark:text-slate-400">
+                <a href="" className="max-w-md text-2xl uppercase mt-6 p-4 rounded-full text-center sm:text-left text-slate-700 dark:text-slate-900 dark:bg-indigo-300">
                     Learn more
                 </a>
             </article>
-            <img className="sm:w-1/2" src="./assets/imgs/team/1.jpg" alt="rocket-dab" />
+            <img className="sm:w-1/2 rounded-3xl opacity-80" src="./assets/internet-computer.png" alt="internet computer" />
         </section>
 
         <hr className="mx-auto bg-black dark:bg-white w-1/2" />
 
-        <section id="about" className="p-6 my-12 scroll-mt-20 widescreen:section-min-height tallscreen:section-min-height">
+        <section id="about" className="p-6 my-12 scroll-mt-20">
             <h2 className="text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-900 dark:text-white">
                 About Us ZimHub
             </h2>
-            <figure className="my-12">
+            <figure className="my-12 flex flex-col-reverse sm:flex-row">
+                <img className="sm:w-1/2 rounded-2xl" src="./assets/group-photo.jpg" alt="internet computer" />
                 <blockquote className="bg-teal-600 dark:bg-black pl-14 pr-8 py-12 rounded-3xl relative">
-                    <p className="text-2xl text-center mt-2 text-white dark:text-slate-400 2">
+                    <p className="text-2xl text-center mt-2 text-white dark:text-slate-100 2">
                         Welcome to Mzanzi Web3! We take pride in South Africa&apos;s burgeoning role as a pivotal contributor to the Internet Computer blockchain within the African continent.
                         In the ever-evolving blockchain realm, new organizations and initiatives consistently make their mark. 
                         Standing at the vanguard of this digital revolution, Mzanzi Web3 (MW3) holds a central position in the region.
@@ -40,9 +42,9 @@ function Home() {
             <h2 className="text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-900 dark:text-white">
                 The <span className="text-indigo-700 dark:text-indigo-300">Internet Computer Protocol</span>
             </h2>
-            <figure className="my-12 flex flex-col-reverse sm:flex-row">
-                <blockquote className="bg-teal-600 dark:bg-black pl-14 pr-8 py-12 rounded-3xl relative">
-                    <p className="text-2xl text-center sm:text-left mt-2 text-white dark:text-slate-400 2">
+            <figure className="my-6 flex flex-col-reverse sm:flex-row">
+                <blockquote className="bg-teal-600 dark:bg-black pl-14 pr-8 py-6 rounded-3xl relative">
+                    <p className="text-2xl text-center sm:text-left mt-2 text-white dark:text-slate-100 2">
                         The Internet Computer Protocol (ICP) heralds a groundbreaking series of protocols that catalyze decentralized synergy between data centers across the globe. 
                         Its audacious mission sets its sights on superseding conventional cloud providers, cultivating an equitable and fortified global computational infrastructure. 
                         The ICP token is the engine of this avant-garde movement, underpinning governance, rewarding community contributors, and enabling smooth transactions. 
@@ -53,37 +55,36 @@ function Home() {
                         Step into the next epoch of decentralized dynamism with the unparalleled prowess of ICP.
                     </p>
                 </blockquote>
-                <img className="sm:w-1/2 mb-6" src="./assets/imgs/testimonial/1/1.png" alt="rocket-dab" />
             </figure>
 
-            <button>Learn more About ICP</button>
+            <button className="dark:bg-slate-600 rounded-full p-6 text-2xl uppercase">Learn more About ICP</button>
 
         </section>
 
         <hr className="mx-auto bg-black dark:bg-white w-1/2" />
 
-        <section id="team" className="p-6 my-12 scroll-mt-20 widescreen:section-min-height tallscreen:section-min-height">
+        <section id="team" className="p-6 my-12 scroll-mt-20">
             <h2 className="text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-900 dark:text-white">
                 Our Team
             </h2>
             <ul className="list-none mx-auto my-12 flex flex-col sm:flex-row items-center gap-4">
                 <li className="w-2/3 sm:w-5/6 flex flex-col items-center py-4 px-2">
-                    <img src="./assets/imgs/blog/1.jpg" alt="Explorer" className="w-2/3 sm:w-5/6 md-6 rounded-3xl shadow-xl" />
-                    <h3 className="text-3xl text-center text-slate-900 dark:text-white">Samuel Muto</h3>
+                    <img src="./assets/same.jpg" alt="dev" className="w-2/3 sm:w-5/6 md-6 rounded-3xl shadow-xl" />
+                    <h3 className="mt-6 text-3xl text-center text-slate-900 dark:text-white">Samuel Muto</h3>
+                    <p className="hidden sm:block text-3xl text-center mt-2 text-slate-900 dark:text-slate-100">Developer Advocate</p>
+                    <p className="sm:hidden text-2xl text-center mt-2 text-slate-500 dark:text-slate-100">Developer</p>
+                </li>
+
+                <li className="w-2/3 sm:w-5/6 flex flex-col items-center py-4 px-2">
+                    <img src="./assets/same.jpg" alt="dev" className="w-2/3 sm:w-5/6 md-6 rounded-3xl shadow-xl" />
+                    <h3 className="mt-6 text-3xl text-center text-slate-900 dark:text-white">Samuel Muto</h3>
                     <p className="hidden sm:block text-3xl text-center mt-2 text-slate-900 dark:text-slate-400">Developer Advocate</p>
                     <p className="sm:hidden text-2xl text-center mt-2 text-slate-500 dark:text-slate-400">Developer</p>
                 </li>
 
                 <li className="w-2/3 sm:w-5/6 flex flex-col items-center py-4 px-2">
-                    <img src="./assets/imgs/blog/1.jpg" alt="Explorer" className="w-2/3 sm:w-5/6 md-6 rounded-3xl shadow-xl" />
-                    <h3 className="text-3xl text-center text-slate-900 dark:text-white">Samuel Muto</h3>
-                    <p className="hidden sm:block text-3xl text-center mt-2 text-slate-900 dark:text-slate-400">Developer Advocate</p>
-                    <p className="sm:hidden text-2xl text-center mt-2 text-slate-500 dark:text-slate-400">Developer</p>
-                </li>
-
-                <li className="w-2/3 sm:w-5/6 flex flex-col items-center py-4 px-2">
-                    <img src="./assets/imgs/blog/1.jpg" alt="Explorer" className="w-2/3 sm:w-5/6 md-6 rounded-3xl shadow-xl" />
-                    <h3 className="text-3xl text-center text-slate-900 dark:text-white">Samuel Muto</h3>
+                    <img src="./assets/same.jpg" alt="dev" className="w-2/3 sm:w-5/6 md-6 rounded-3xl shadow-xl" />
+                    <h3 className="mt-6 text-3xl text-center text-slate-900 dark:text-white">Samuel Muto</h3>
                     <p className="hidden sm:block text-3xl text-center mt-2 text-slate-900 dark:text-slate-400">Developer Advocate</p>
                     <p className="sm:hidden text-2xl text-center mt-2 text-slate-500 dark:text-slate-400">Developer</p>
                 </li>
@@ -92,19 +93,25 @@ function Home() {
 
         <hr className="mx-auto bg-black dark:bg-white w-1/2" />
 
-        <section id="events" className="p-6 my-12 scroll-mt-20 widescreen:section-min-height tallscreen:section-min-height">
+        <section id="events" className="p-6 my-12 scroll-mt-20">
             <h2 className="text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-900 dark:text-white">
-                Events
+                Out Upcoming <span className="dark:text-indigo-300">Events</span>
             </h2>
+            <img src="./assets/group-photo.jpg" className="rounded-2xl my-14 w-full" />
             
         </section>
 
         <hr className="mx-auto bg-black dark:bg-white w-1/2" />
 
-        <section id="blog" className="p-6 my-12 scroll-mt-20 widescreen:section-min-height tallscreen:section-min-height">
-            <h2 className="text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-900 dark:text-white">
+        <section id="blog" className="p-6 my-12 place-items-center grid scroll-mt-20">
+            <h2 className="text-4xl font-bold text-center sm:text-5xl mb-14 text-slate-900 dark:text-white">
                 Recent Blogs
             </h2>
+            <div className="container grid grid-cols-1 gap-8 my-auto lg:grid-cols-2">
+                <Blog />
+                <Blog />
+                <Blog />
+            </div>
             
         </section>
     </main>
