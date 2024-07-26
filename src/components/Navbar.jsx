23ccc5e68502533d18ceb093e563e56cc04cad94
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-07-26 08:19:56 +0200

    fix
    fa icons now showing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,7 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+
 function Navbar() {
     return (
         <nav className="h-20 bg-gray-100">
@@ -5,35 +9,30 @@ function Navbar() {
                 <div className="flex space-x-3">
                     {/* <img src="" alt="Car Logo" width="20%" /> */}
                     <a href="/" className="inline-block py-4 md:py-0 mt-2">
-                        <span className="p-1 text-2xl leading-none text-gray-900 font-mono  text-clip">Logo</span>
+                        <span className="p-1 text-2xl leading-none text-gray-900 font-mono text-clip">Logo</span>
                     </a>
                 </div>
 
                 <div className="flex space-x-10 font-mono text-black">
-
                     <a href="/" className="hover:font-bold hover:text-gray-900 transition">Home</a>
-
                 </div>
 
                 <div className="flex space-x-5">
-
                     <a href="/">
-                        <i className="text-black hover:text-gray-700 fa-brands fa-twitter text-lg hover:text-xl transition"></i>
+                        <FontAwesomeIcon icon={faTwitter} className="text-gray-500 hover:text-gray-700 text-lg hover:text-xl transition" />
                     </a>
                     <a href="/">
-                        <i className="text-gray-500 hover:text-gray-700 fa-brands fa-facebook text-lg hover:text-xl transition"></i>
+                        <FontAwesomeIcon icon={faFacebook} className="text-gray-500 hover:text-gray-700 text-lg hover:text-xl transition" />
                     </a>
                     <a href="/">
-                        <i className="text-gray-500 hover:text-gray-700 fa-solid fa-globe text-lg hover:text-xl transition"></i></a>
+                        <FontAwesomeIcon icon={faGlobe} className="text-gray-500 hover:text-gray-700 text-lg hover:text-xl transition" />
+                    </a>
                     <a href="/">
-                        <i className="text-gray-500 hover:text-gray-700 fa-brands fa-instagram text-lg hover:text-xl transition"></i>
+                        <FontAwesomeIcon icon={faInstagram} className="text-gray-500 hover:text-gray-700 text-lg hover:text-xl transition" />
                     </a>
                 </div>
-
             </div>
         </nav>
-
-
     )
 }
 
